9e74054508c06e56b5d457dde69561bba37a876c
owolabiadebayo <owolabiadebayo@punch.cool>
2023-11-10 22:53:28 +0100

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-  "https://idverification.ng",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
   "https://geosoft-nimc-portal.netlify.app",
+  // "http://127.0.0.1:5500",
+  // "http://localhost:3500",
+  // "http://localhost:3000",
+  // "https://geosoft-nimc-portal.netlify.app",
 ];
 
 module.exports = allowedOrigins;
