4da95b3061cffbfe2ca65c29de8aee0c1013dd8c
Owolabi adebayo Stephen <owolabiadebayo78@yahoo.com>
2024-12-05 19:42:29 +0100

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const BASE_URL = "https://geosoft-nimc-portal-backend-1.onrender.com/api";
+const BASE_URL = "https://geosoft-nimc-portal-backend-2q01mugrn-owolabiadebayos-projects.vercel.app/api";
 
 module.exports = axios.create({
   baseURL: BASE_URL,
